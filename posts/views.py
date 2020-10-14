e82306b5ebd8597911d0444dde8892d8ec0abaec
Ram <twinka4ik174@mail.ru>
2020-10-14 21:47:39 +0500

    s

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import get_object_or_404, render
 
+from django.shortcuts import redirect,render
+#  импортируем CreateView, чтобы создать ему наследника
+from django.views.generic import CreateView
+#  функция reverse_lazy позволяет получить URL по параметру "name" функции path()
+from django.urls import reverse_lazy
 
+#  импортируем класс формы, чтобы сослаться на неё во view-клаccf
+from .forms import PostForm
 from .models import Group, Post
+from posts.forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy("login") #  где login — это параметр "name" в path()
+    template_name = "signup.html" 
 
 
 def group_posts(request, slug):
@@ -17,4 +31,24 @@ def index(request):
     return render(request, "index.html", {
         "posts": latest,
     })
+    
+def new_post(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        
+    if form.is_valid():
+        form.save()
+        return redirect('http://127.0.0.1:8000/')
+        return render(request, "new.html", {
+                    "form": form,
+            })   
+        
+        
+	
+    else:  
+        form = PostForm()
+    return render(request, 'new.html', {  
+						    'form': form
+                    })
     
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from posts import views
+
 
 urlpatterns = [
     # path() для страницы регистрации нового пользователя
