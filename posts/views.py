8472d63819804b5f1b8cc2b7f96d4b367683d6b8
Ram <twinka4ik174@mail.ru>
2020-10-16 16:08:05 +0500

    s

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,14 @@
-from django.contrib.auth.decorators import get_user_model, login_required
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect, render
 
-from django.shortcuts import get_object_or_404, render
-from django.shortcuts import redirect,render
 #  импортируем CreateView, чтобы создать ему наследника
 from django.views.generic import CreateView
 #  функция reverse_lazy позволяет получить URL по параметру "name" функции path()
 from django.urls import reverse_lazy
 #  импортируем класс формы, чтобы сослаться на неё во view-клаccf
+
 from .forms import PostForm
 from .models import Group, Post
 from posts.forms import CreationForm
@@ -33,38 +35,21 @@ def index(request):
     return render(request, "index.html", {
         "posts": latest,
     })
-
+# @login_required - testing user authorization
 @login_required
 def new_post(request):
-#if not request.user.is_authenticated:
-#form = PostForm()
-#return redirect('signup') 
-    
+    form = PostForm(request.POST or None)
     if request.method == 'POST':
-        form = PostForm(request.POST)
-        
         if form.is_valid():
-            author = User.objects.get(username=user)
-            form.save() 
-            return render(request, 'new.html', {  
-                'form': form
-            })
-
-            return redirect('index') 
-
-        
-               
-    form = PostForm()               
-    return render(request, 'new.html', {  
-                'form': form
-            })
-
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('index')
+    return render(request, 'new.html', {'form': form})
     if request.method == 'GET':
         form = PostForm(request.GET)
         if form.is_valid():
-            form.save()
+            form.save(form)
             return redirect('index')
     form = PostForm()
-    return render(request, 'new.html', {  
-                'form': form
-            })       
+    return render(request, 'new.html', {'form': form})       
