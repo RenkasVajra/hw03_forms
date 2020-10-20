7f896f2d1e2aa2d76583dc89020bcc1aa7ad5f03
Ram <twinka4ik174@mail.ru>
2020-10-20 12:44:39 +0500

    s

--- posts/views.py
+++ posts/views.py
@@ -24,11 +24,11 @@ def index(request):
 @login_required
 def new_post(request): 
     form = PostForm(request.POST or None)
-    if not request.method == 'POST':
-        if not form.is_valid():
-            return redirect('index')
+    if not form.is_valid():
+        return render(request, 'new.html', {'form': form}) 
 
     post = form.save(commit=False)
     post.author = request.user
     post.save()
-    return render(request, 'new.html', {'form': form})   
+      
+    return redirect('index')
